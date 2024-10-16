570b41e36be9167275c01a1d136b85f5d1bb3fcd
Michal Cygankiewicz <m.cygankiewicz@amtsolution.pl>
2024-10-16 10:49:07 +0200

    Authentication

--- app.py
+++ app.py
@@ -5,6 +5,8 @@ load_dotenv()
 from backend import search_elasticsearch, get_unique_terms_and_count, find_similar_records
 from news import display_results
 
+CORRECT_PASSWORD = os.getenv("CORRECT_PASSWORD")
+
 def load_news():
     import news
     news.run()
@@ -22,52 +24,66 @@ def create_selectbox(key, label, placeholder, data_fetch_function, *args):
     select = st.selectbox(label, options)
     return select
 
+def check_password(password):
+    if password == CORRECT_PASSWORD:
+        st.session_state.authenticated = True
+    else:
+        st.error("Incorrect password. Please try again.")
+
 # Streamlit frontend
 def main():
-    st.sidebar.image(os.getenv('LOGO'), use_column_width=True)
-    st.sidebar.title("Choose action:")
-    semantic = st.sidebar.button("Semantic search")
-    organizations = st.sidebar.button("Organizations summary")
-    topics = st.sidebar.button("Topic summary")
-
-    # Variable to track which button is active
-    if semantic:
-        st.session_state['active'] = 1
-    elif organizations:
-        st.session_state['active'] = 2
-    elif topics:
-        st.session_state['active'] = 3
-
-    if 'active' in st.session_state:
-        if st.session_state['active'] == 1:
-            query = st.text_input("Enter your search query for semantic search and press button below:")
-            search = st.button("Search")
-            if search:
-                if query:
-                    # Display search results
-                    with st.spinner("Searching..."):
-                        results = search_elasticsearch(query)
-                        if results:
-                            display_results(results)
-                        else:
-                            st.warning("No results found!")
-                else:
-                    st.warning("Please enter a search query.")
-        elif st.session_state['active'] == 2:
-            company = create_selectbox(
-                "company",
-                'Choose an organization - so that you will get news that are related to that organization as well as some explanation:',
-                '',
-                get_unique_terms_and_count,
-                os.getenv('ELASTIC_INDEX'),
-                'entities.keyword'
-                )
-            load_company(company)
-        elif st.session_state['active'] == 3:
-            topic = create_selectbox("topic", 'Choose a topic - so that you will get news that are related to that topic as well as some explanation:', '', get_unique_terms_and_count, os.getenv('ELASTIC_INDEX'), 'topics.keyword')
-            load_topic(topic)
+    if "authenticated" not in st.session_state:
+        st.session_state.authenticated = False
+    if not st.session_state.authenticated:
+        st.title("🔒 Please Enter Your Password")
+        password = st.text_input("Password", type="password")
+        if st.button("Submit"):
+            check_password(password)
     else:
-        st.header("Choose action from left menu.")
+        st.sidebar.image(os.getenv('LOGO'), use_column_width=True)
+        st.sidebar.title("Choose action:")
+        semantic = st.sidebar.button("Semantic search")
+        organizations = st.sidebar.button("Organizations summary")
+        topics = st.sidebar.button("Topic summary")
+
+        # Variable to track which button is active
+        if semantic:
+            st.session_state['active'] = 1
+        elif organizations:
+            st.session_state['active'] = 2
+        elif topics:
+            st.session_state['active'] = 3
+
+        if 'active' in st.session_state:
+            if st.session_state['active'] == 1:
+                query = st.text_input("Enter your search query for semantic search and press button below:")
+                search = st.button("Search")
+                if search:
+                    if query:
+                        # Display search results
+                        with st.spinner("Searching..."):
+                            results = search_elasticsearch(query)
+                            if results:
+                                display_results(results)
+                            else:
+                                st.warning("No results found!")
+                    else:
+                        st.warning("Please enter a search query.")
+            elif st.session_state['active'] == 2:
+                company = create_selectbox(
+                    "company",
+                    'Choose an organization - so that you will get news that are related to that organization as well as some explanation:',
+                    '',
+                    get_unique_terms_and_count,
+                    os.getenv('ELASTIC_INDEX'),
+                    'entities.keyword'
+                    )
+                load_company(company)
+            elif st.session_state['active'] == 3:
+                topic = create_selectbox("topic", 'Choose a topic - so that you will get news that are related to that topic as well as some explanation:', '', get_unique_terms_and_count, os.getenv('ELASTIC_INDEX'), 'topics.keyword')
+                load_topic(topic)
+        else:
+            st.header("Choose action from left menu.")
 
 
 #    if "topic" not in st.query_params and "company" not in st.query_params and "news" not in st.query_params:
